9283a86828404166c5e3a9ac261da17cbcfa6b6c
Alan Ma <alan_ma@live.ca>
2018-09-15 15:24:38 -0400

    added a header

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// Initialize the platform object:
+var platform = new H.service.Platform({
+'app_id': 'fF1mVXDKnAlRMVVwdHuO',
+'app_code': 'KyO0DGou-M7uza7g6WGlpQ'
+});
+
+// Obtain the default map types from the platform object
+var layers = platform.createDefaultLayers();
+
+// Instantiate (and display) a map object:
+var map = new H.Map(
+document.getElementById('mapContainer'),
+layers.normal.xbase,
+{
+  zoom: 3,
+  center: { lng: -100, lat: 40 }
+});
+
+var mapEvents = new H.mapevents.MapEvents(map);
+
+map.addEventListener('tap', function(evt) {
+  console.log(evt.type, evt.currentPointer.type);
+});
+
+var behavior = new H.mapevents.Behavior(mapEvents);
+
+var ui = H.ui.UI.createDefault(map, layers);
+
